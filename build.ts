0a80742473ce754a2aba1c9f1936fc89f190db06
Mick Negenman <36791@ma-web.nl>
2024-09-02 16:04:33 +0200

    Improved start command

---
+++ build.ts
@@ -0,0 +1,8 @@
+const glob = new Bun.Glob("*.ts");
+const files = [ ...glob.scanSync({ cwd: './src/client/ts/', absolute: true }) ];
+
+Bun.build({
+    entrypoints: files,
+    outdir: './src/client/compiled/',
+    minify: true
+});
